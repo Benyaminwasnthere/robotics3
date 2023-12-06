c0a6022e14f486b94527ba4701aa1b084c3718a1
Benyaminwasnthere <124644188+Benyaminwasnthere@users.noreply.github.com>
2023-12-06 00:16:03 -0500

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -2,6 +2,33 @@ import argparse
 import os
 
 import numpy as np
+#-----------------------------------
+# Function to generate odometry measurements
+def generate_odometry_measurements(controls_file_path, sigma_e_omega, sigma_e_phi):
+    control_sequence = np.load(controls_file_path, allow_pickle=True)
+    executed_controls=[]
+    # Extract initial pose and controls
+    initial_pose = control_sequence[0]  # Use [0] directly for the initial pose
+    planned_controls = control_sequence[1:]  # Use slicing to get the planned controls
+
+
+
+    odometry_measurements = []
+    current_pose = np.array(initial_pose)
+
+    for control in planned_controls:
+        # Extract control values
+        v, phi = control
+
+        # Implement Eq. 5 to generate odometry measurements with noise
+        noisy_omega = v + np.random.normal(0, sigma_e_omega)
+        noisy_phi = phi + np.random.normal(0, sigma_e_phi)
+
+
+        # Append the noisy odometry measurement
+        odometry_measurements.append([noisy_omega, noisy_phi])
+    odometry_measurements.insert(0, initial_pose)
+    return odometry_measurements
 
 def create_executed(controls_file_path):
     # Load control inputs from the file
@@ -39,8 +66,27 @@ def simulate(plan_file, map_file, execution_file, sensing_file):
     control_sequence = np.array(executed_controls, dtype=object)
     np.save(os.path.join("gts", execution_file), control_sequence)
     print(f"Made Actuation Model: {execution_file} ")
+    # Low-level noise
+    sigma_e_omega_low = 0.05
+    sigma_e_phi_low = 0.1
+    measurements_low = generate_odometry_measurements(f"gts/{execution_file}", sigma_e_omega_low, sigma_e_phi_low)
+    control_sequence = np.array(measurements_low, dtype=object)
+    np.save(os.path.join("readings", f"{sensing_file}_L.npy"), control_sequence)
+    # High-level noise
+    sigma_e_omega_high = 0.1
+    sigma_e_phi_high = 0.3
+    measurements_high = generate_odometry_measurements(f"gts/{execution_file}", sigma_e_omega_high, sigma_e_phi_high)
+    control_sequence = np.array( measurements_high, dtype=object)
+    np.save(os.path.join("readings", f"{sensing_file}_H.npy"), control_sequence)
+
+    print(f"Made Odometry model LOW: {sensing_file}_L.npy ")
+    print(f"Made Odometry model HIGH: {sensing_file}_H.npy ")
+
+
+
 
 if __name__ == "__main__":
+    #input : --plan controls_X_Y.npy --map landmark_X.npy --execution gt_X_Y.npy --sensing readings_X_Y
     # Create argument parser
     parser = argparse.ArgumentParser(description="Simulate something.")
 
